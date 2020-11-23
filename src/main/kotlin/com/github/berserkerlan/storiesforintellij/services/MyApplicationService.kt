0951fce64a35d48b3a7bf2d86bba0b35c7fb1b76
GitHub Action <action@github.com>
2020-11-23 09:53:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/berserkerlan/storiesforintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.berserkerlan.storiesforintellij.services
+
+import com.github.berserkerlan.storiesforintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
